4682cdb31de385551eb890c1e344f4550b6eb5f8
Motiejus Jakštys <desired.mta@gmail.com>
2019-10-26 14:50:01 +0300

    add airports.dat

--- MTM/task4-straipsnis/schema.sql
+++ MTM/task4-straipsnis/schema.sql
@@ -6,10 +6,10 @@ CREATE TABLE airports (
     iata TEXT,
     icao TEXT,
     geom GEOMETRY,
-    latitude DOUBLE,
-    longitude DOUBLE,
-    altitude DOUBLE,
-    CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(geom) = 2),
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    altitude DOUBLE PRECISION,
+    CONSTRAINT enforce_dims_geom CHECK (st_ndims(geom) = 2),
     CONSTRAINT enforce_geotype_geom CHECK (geometrytype(geom) = 'POINT'::text OR geom IS NULL),
-    CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 4326)
+    CONSTRAINT enforce_srid_geom CHECK (st_srid(geom) = 4326)
 );
