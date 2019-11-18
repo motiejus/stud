bcaee932bd4eac2e238a0cb47296ee2b1887986d
Motiejus Jakštys <desired.mta@gmail.com>
2019-11-18 12:00:08 +0200

    finish measure.py

--- Karto/assignment3/deg.py
+++ Karto/assignment3/deg.py
@@ -1,22 +1,22 @@
-from decimal import Decimal
+from decimal import Decimal as D
 
 class Deg:
     def __str__(self):
         return "%03d-%02d-%04.2f" % (self.deg, self.mm, self.ss)
 
     def __init__(self, deg, mm, ss):
-        self.deg = Decimal(deg)
-        self.mm = Decimal(mm)
-        self.ss = Decimal(ss)
+        self.deg = D(deg)
+        self.mm = D(mm)
+        self.ss = D(ss)
 
     @classmethod
     def from_1(cls, deg):
-        assert isinstance(deg, Decimal)
+        assert isinstance(deg, D)
 
         pdeg, pmm = divmod(deg, 1)
-        pmm = pmm * Decimal(60)
+        pmm = pmm * D(60)
         pmm, pss = divmod(pmm, 1)
-        pss = pss * Decimal(60)
+        pss = pss * D(60)
         return cls(pdeg, pmm, pss)
 
     @classmethod
@@ -24,22 +24,31 @@ class Deg:
         if '-' in instr:
             deg, mm, ss = instr.split('-')
             return Deg(deg, mm, ss)
-        return cls.from_1(Decimal(instr))
+        return cls.from_1(D(instr))
 
+    @property
     def frac(self):
         return (
             self.deg +
-            self.mm / Decimal(60) +
-            self.ss / Decimal(3600)).normalize()
+            self.mm / D(60) +
+            self.ss / D(3600)).normalize()
 
 
 import unittest
 class TestDeg(unittest.TestCase):
     def test_deg(self):
-        w = Deg.guess('125.5')
+        w = Deg.guess('125.505')
         self.assertEqual(125, w.deg)
         self.assertEqual(30, w.mm)
-        self.assertEqual(0, w.ss)
+        self.assertEqual(18, w.ss)
+
+    def test_guess(self):
+        w = Deg.guess('112-58-25.4')
+        self.assertEqual(112, w.deg)
+        self.assertEqual(58, w.mm)
+        self.assertEqual(D('25.4'), w.ss)
+        frac = "%3.7f" % w.frac
+        self.assertEqual('112.9737222', frac)
 
     def test_str_lower(self):
         w = Deg(120, 1, 42.541)
