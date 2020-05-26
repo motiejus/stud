6d3fcd2ff0633dfd3b0c916f3f9151ccb21e0000
Motiejus Jakštys <desired.mta@gmail.com>
2020-05-26 17:18:12 +0300

    add rectangle

--- II/Referatas/layer2img.py
+++ II/Referatas/layer2img.py
@@ -5,6 +5,7 @@ import argparse
 import geopandas
 import psycopg2
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 INCH = 25.4  # mm
 
@@ -32,6 +33,9 @@ def parse_args():
     parser.add_argument(
             '--clip', type=float, nargs=4,
             metavar=('xmin', 'ymin', 'xmax', 'ymax'))
+    parser.add_argument(
+            '--rect', type=float, nargs=4, help="Overlay a rectangle",
+            metavar=('xmin', 'ymin', 'xmax', 'ymax'))
     return parser.parse_args()
 
 
@@ -45,9 +49,17 @@ def main():
         f = geopandas.read_file(args.infile)
     fig, ax = plt.subplots()
     f.plot(ax=ax, figsize=args.size)
-    if args.clip:
-        ax.set_xlim(left=args.clip[0], right=args.clip[2])
-        ax.set_ylim(bottom=args.clip[1], top=args.clip[3])
+    if c := args.clip:
+        ax.set_xlim(left=c[0], right=c[2])
+        ax.set_ylim(bottom=c[1], top=c[3])
+    if r := args.rect:
+        w, h = r[2] - r[0], r[3] - r[1]
+        rect = patches.Rectangle(
+                (r[0], r[1]),
+                w, h,
+                linewidth=1,edgecolor='r',facecolor='none')
+        ax.add_patch(rect)
+
     ax.axis('off')
     ax.margins(0, 0)
     fig.tight_layout(0)
