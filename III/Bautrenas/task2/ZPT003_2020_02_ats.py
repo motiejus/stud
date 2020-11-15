4765e589a6b97650cfdf0351f50c2f6217e872e2
Motiejus Jakštys <motiejus@uber.com>
2020-11-15 15:39:05 +0200

    moar

--- III/Bautrenas/task2/ZPT003_2020_02_ats.py
+++ III/Bautrenas/task2/ZPT003_2020_02_ats.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+
+storgalio_apimtis=119.4
+plongalio_apimtis=46.2
+rastu_ilgis=5.38
+storgalio_zieves_storis=10.6
+plongalio_zieves_storis=10.5
+storgalio_skersmuo_su_zieve=int(storgalio_apimtis+storgalio_zieves_storis*2)
+plongalio_skersmuo_su_zieve=int(plongalio_apimtis+plongalio_zieves_storis*2)
 
 
-print("""
+tpl="""
 Uzduoties Nr. 
 ZPT003_2020_02
 Skaiciavo(Pavarde Vardas):
@@ -9,34 +18,34 @@ Jakstys_Motiejus
 Gaminamo popieriaus pavadinimas pvz. ULTRA/62 (duota) 
 Ultra3C
 Gaminamo popieriaus lapo plotis W (mm) 
-*******
+750
 Gaminamo popieriaus lapo ilgis L (mm) 
-*******
+900
 Gaminamo popieriaus metro kv. svoris  (g/m2) 
-*******
+95
 1.   Popierius gaminamas is medienos pvz. AZUOLAS (duota) 
-*******
+Baltalksnis
 Popieriaus gamybos metodas (nurodytas) 
-*******
+Cheminis/Sulfitinis
 Rastu kiekis (duotas)
-*******
+5
 Storgalio apimtis cm (0.0) (duota)
-*******
+{storgalio_apimtis:.1f}
 Plongalio apimtis cm (0.0) (duota)
-*******
+{plongalio_apimtis:.1f}
 Rastu ilgis (duotas)
-*******
+{rastu_ilgis:.2f}
 == Skaiciavimai ==
 Apskaiciuotas STORGALIO skersmuo su zieve (0.00 cm tikslumu)
-*******
+{storgalio_skersmuo_su_zieve:.2f}
 Storgalio zieves storis (is lenteles 0.0 mm tikslumu)
-*******
+{storgalio_zieves_storis:.1f}
 Storgalio skersmuo (be zv.) su pataisa del rasto nelygumo (0.00 cm tikslumu)
 *******
 Apskaiciuotas PLONGALIO skersmuo su zieve (0.00 cm tikslumu)
-*******
+{plongalio_skersmuo_su_zieve:.2f}
 Plongalio zieves storis (is lenteles 0.0 mm tikslumu)
-*******
+{plongalio_zieves_storis:.1f}
 Plongalio (be zv.) skersmuo su pataisa del rasto nelygumo (0.00 cm tikslumu)
 *******
 VIENO rasto turis (0.000 m3 tikslumu)
@@ -201,4 +210,6 @@ kartono gamybos kaina Eu (duota)
 *******
 Vieno kartono lapo savikaina (0.00 Eu) 
 *******
-""")
+"""
+
+print(tpl.format(**(dict(locals()))))
