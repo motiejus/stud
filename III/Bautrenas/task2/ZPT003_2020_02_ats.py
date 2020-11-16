1ff7303377b526c238842c402040688f1b370ff4
Motiejus Jakštys <motiejus@uber.com>
2020-11-16 22:52:47 +0200

    t2

--- III/Bautrenas/task2/ZPT003_2020_02_ats.py
+++ III/Bautrenas/task2/ZPT003_2020_02_ats.py
@@ -84,47 +84,73 @@ Vieno gaminamo lapo plotas (0.000 m2 tikslumu)
 Pagaminta popieriaus lapu (vnt.)
 {t1_pagaminta_popieriaus_lapu}"""
 print(tpl1.format(**(dict(locals()))))
+
+t2_storgalio_apimtis_su_zieve=136.7 # cm
+t2_plongalio_apimtis_su_zieve=58.7  # cm
+t2_storgalio_skersmuo_su_zieve=t2_storgalio_apimtis_su_zieve/pi  # cm
+t2_plongalio_skersmuo_su_zieve=t2_plongalio_apimtis_su_zieve/pi  # cm
+
+t2_rastu_kiekis=3
+t2_rastu_ilgis=5.38 # m
+t2_storgalio_zieves_storis=10.6 # mm
+t2_plongalio_zieves_storis=10.5 # mm
+t2_storgalio_skersmuo_be_zieves=round(t2_storgalio_skersmuo_su_zieve-t2_storgalio_zieves_storis/10*2+.5) # cm
+t2_plongalio_skersmuo_be_zieves=round(t2_plongalio_skersmuo_su_zieve-t2_plongalio_zieves_storis/10*2+.5) # cm
+
+t2_vieno_rasto_turis=nupjauto_kugio_turis(t2_rastu_ilgis, t2_plongalio_skersmuo_be_zieves/100, t2_storgalio_skersmuo_be_zieves/100) # m3
+t2_visu_rastu_turis=t2_vieno_rasto_turis*t2_rastu_kiekis # m3
+t2_medienos_svoris=800 # kg/m3
+t2_visu_rastu_svoris=t2_visu_rastu_turis*t2_medienos_svoris # kg
+t2_gaunamas_popieriaus_kiekis_perc=6.82 # [0-100]%
+t2_gaunamas_popieriaus_kiekis_trupm=t2_gaunamas_popieriaus_kiekis_perc/100 # [0-1]
+t2_medienos_svoris_popieriaus_gaminimui=t2_visu_rastu_svoris*t2_gaunamas_popieriaus_kiekis_trupm # kg
+t2_popieriaus_tankis=0.09 # kg/m2
+t2_viso_pagaminto_popieriaus_plotas=t2_medienos_svoris_popieriaus_gaminimui/t2_popieriaus_tankis # m2
+t2_vieno_lapo_plotas=.75*.9 # m2
+t2_pagaminta_popieriaus_lapu=int(t2_viso_pagaminto_popieriaus_plotas/t2_vieno_lapo_plotas) # kiekis
+
+
 tpl2="""2.   Popierius gaminamas is medienos pvz. AZUOLAS (duota) 
-*******
+Egle
 Popieriaus gamybos metodas (nurodytas) 
-*******
+Mechaninis/Cheminis
 Rastu kiekis (duotas)
-*******
+3
 Storgalio apimtis cm (0.0) (duota)
-*******
+{t2_storgalio_apimtis_su_zieve:.1f}
 Plongalio apimtis cm (0.0) (duota)
-*******
+{t2_plongalio_apimtis_su_zieve:.1f}
 Rastu ilgis (duotas)
-*******
+{t2_rastu_ilgis:.2f}
 == Skaiciavimai ==
 Apskaiciuotas STORGALIO skersmuo su zieve (0.00 cm tikslumu)
-*******
+{t2_storgalio_skersmuo_su_zieve:.2f}
 Storgalio zieves storis (is lenteles 0.0 mm tikslumu)
-*******
+{t2_storgalio_zieves_storis:.1f}
 Storgalio skersmuo (be zv.) su pataisa del rasto nelygumo (0.00 cm tikslumu)
-*******
+{t2_storgalio_skersmuo_be_zieves:.2f}
 Apskaiciuotas PLONGALIO skersmuo su zieve (0.00 cm tikslumu)
-*******
+{t2_plongalio_skersmuo_su_zieve:.2f}
 Plongalio zieves storis (is lenteles 0.0 mm tikslumu)
-*******
+{t2_plongalio_zieves_storis:.1f}
 Plongalio (be zv.) skersmuo su pataisa del rasto nelygumo (0.00 cm tikslumu)
-*******
+{t2_plongalio_skersmuo_be_zieves:.2f}
 VIENO rasto turis (0.000 m3 tikslumu)
-*******
+{t2_vieno_rasto_turis:.3f}
 VISU rastu turis (0.000 m3 tikslumu)
-*******
+{t2_visu_rastu_turis:.3f}
 Visu rastu Medienos svoris (0.000 kg tikslumu)
-*******
+{t2_visu_rastu_svoris:.3f}
 Gaunamas popieriaus kiekis priklausomai muo gamybos metodo (% duota)
-*******
+{t2_gaunamas_popieriaus_kiekis_perc:.2f}
 Medienos svoris popieriaus gaminimui (0.000 kg tikslumu)
-*******
+{t2_medienos_svoris_popieriaus_gaminimui:.3f}
 Viso pagaminto popieriaus plotas (0.000 m2 tikslumu)
-*******
+{t2_viso_pagaminto_popieriaus_plotas:.3f}
 Vieno gaminamo lapo plotas (0.000 m2 tikslumu)
-*******
+{t2_vieno_lapo_plotas:.3f}
 Pagaminta popieriaus lapu (vnt.)
-*******
+{t2_pagaminta_popieriaus_lapu}
 == APIBENDRINIMAS == 
 Isviso pagaminta popieriaus lapu (vnt.) 
 *******
