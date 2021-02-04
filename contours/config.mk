34f72686888ea8b46cce95b811c4ace4acf705d2
Motiejus Jakštys <motiejus@uber.com>
2021-02-04 18:05:24 +0200

    remove karto2020; contour bounds

--- contours/config.mk
+++ contours/config.mk
@@ -1,6 +1,7 @@
 #krekenava
 BOUNDS = xmin=504440 ymin=6154741 xmax=507410 ymax=6158941
 BOUNDS = xmin=493674 ymin=6146103 xmax=518869 ymax=6171741
+BOUNDS = xmin=505495 ymin=6156392 xmax=506352 ymax=6157227
 VARIANT = 2009
 
 #uzupis
